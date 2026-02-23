3ad2145f50e4cf43d44f7cf5034255f6d403b51b
Rafael Neves <rafael.nsouza@goias.gov.br>
2026-02-23 16:26:07 -0300

    refactor(model): Remove auditoria e simplifica a classe PessoaJuridica

--- loja-virtual/src/main/java/com/github/rafsnow/lojavirtual/model/PessoaJuridica.java
+++ loja-virtual/src/main/java/com/github/rafsnow/lojavirtual/model/PessoaJuridica.java
@@ -1,34 +1,32 @@
 package com.github.rafsnow.lojavirtual.model;
 
-import java.time.LocalDateTime;
-
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "pessoa_juridica")
-@SequenceGenerator(name = "seq_pessoa_juridica", sequenceName = "seq_pessoa_juridica", allocationSize = 1, initialValue = 1)
 @Getter
 @Setter
-@EntityListeners(AuditingEntityListener.class)
-public class PessoaJuridica {
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+public class PessoaJuridica extends Pessoa {
 
   @Id
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_pessoa_juridica")
+  @SequenceGenerator(name = "seq_pessoa_juridica", sequenceName = "seq_pessoa_juridica", allocationSize = 1, initialValue = 1)
+  @EqualsAndHashCode.Include
   private Long id;
 
   @NotBlank(message = "O campo CNPJ é obrigatório")
@@ -50,16 +48,4 @@ public class PessoaJuridica {
 
   @Column(name = "categoria")
   private String categoria;
-
-  @CreatedDate
-  @Column(name = "data_criacao", nullable = false, updatable = false)
-  private LocalDateTime dataCriacao;
-
-  @LastModifiedDate
-  @Column(name = "data_atualizacao")
-  private LocalDateTime dataAtualizacao;
-
-  @LastModifiedBy
-  @Column(name = "usuario_atualizacao")
-  private String usuarioAtualizacao;
 }
