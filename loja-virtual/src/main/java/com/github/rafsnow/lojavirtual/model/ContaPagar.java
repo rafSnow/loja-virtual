8839617451baacb5b938f29dfca64280f64311c2
Rafael Neves <rafael.neves.souzaa@gmail.com>
2026-02-24 11:41:11 -0300

    feat(model): Adiciona anotações JPA para a entidade ContaPagar

--- loja-virtual/src/main/java/com/github/rafsnow/lojavirtual/model/ContaPagar.java
+++ loja-virtual/src/main/java/com/github/rafsnow/lojavirtual/model/ContaPagar.java
@@ -7,9 +7,15 @@ import com.github.rafsnow.lojavirtual.enums.StatusContaPagar;
 
 import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -26,10 +32,15 @@ import lombok.Setter;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
 public class ContaPagar extends Auditoria {
 
+  @Id
+  @SequenceGenerator(name = "seq_conta_pagar", sequenceName = "seq_conta_pagar", allocationSize = 1, initialValue = 1)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_pagar")
+  @EqualsAndHashCode.Include
   private Long id;
 
   private String descricao;
 
+  @Enumerated(EnumType.STRING)
   private StatusContaPagar status;
 
   private LocalDate dataVencimento;
